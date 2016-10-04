a02f6e90c1bb6a66bf99890cfddfd2cf779a8390
Brad <brad@brad.tw>
2016-10-04 19:05:13 +0800

    v4

--- app/src/main/java/brad/tw/myiotest/MainActivity.java
+++ app/src/main/java/brad/tw/myiotest/MainActivity.java
@@ -1,12 +1,21 @@
 package brad.tw.myiotest;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -16,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
     private TextView tv;
+
+    private File sdroot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +38,38 @@ public class MainActivity extends AppCompatActivity {
         sp = getSharedPreferences("gamedata", MODE_PRIVATE);
         editor = sp.edit();
 
+        // check permission
+        if (Build.VERSION.SDK_INT>=23) {
+            checkPermission();
+        }
+
+
+        sdroot = Environment.getExternalStorageDirectory();
+        Log.v("brad", sdroot.getAbsolutePath());
+
     }
+
+    private void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            // 願意
+        }
+
+    }
+
     public void test1(View v){
         editor.putString("user", "Brad");
         editor.putBoolean("sound", true);
@@ -68,4 +110,13 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+    public void test5(View v){
+
+
+    }
+    public void test6(View v){
+
+    }
+
 }
